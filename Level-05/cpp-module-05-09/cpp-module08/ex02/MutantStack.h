70f5027796a0ae9dd7b4173499e658e4359442e8
shrimech <eddinen77@gmail.com>
2026-08-09 14:54:43 +0100

    fix: ex02-cpp08

--- Level-05/cpp-module-05-09/cpp-module08/ex02/MutantStack.h
+++ Level-05/cpp-module-05-09/cpp-module08/ex02/MutantStack.h
@@ -12,12 +12,19 @@ class MutantStack : public std::stack<T> {
         // Define iterator types
        typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+       typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
 
         // Begin and end functions for iteration
         iterator begin() { return this->c.begin(); }
         iterator end() { return this->c.end(); }
         const_iterator begin() const { return this->c.begin(); }
-        const_iterator end() const { return this->c.end(); }
+        const_iterator end() const { return this->c.end();}
+        reverse_iterator rbegin() { return this->c.rbegin(); }
+        reverse_iterator rend() { return this->c.rend(); }
+        const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+        const_reverse_iterator rend() const { return this->c.rend(); }
 };
 
 
